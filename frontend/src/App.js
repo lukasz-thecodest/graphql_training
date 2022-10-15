494b30bbe18cb4678f1f5521485933a5a3987733
= <=>
2022-10-15 07:09:40 +0200

    login functionality in FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,51 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
+import AuthContext from "./context/auth-context";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [tokenExpiration, setTokenExpiration] = useState(null);
+
+  const login = (token, userId, tokenExpiration) => {
+    setToken(token);
+    setUserId(userId);
+    setTokenExpiration(tokenExpiration);
+  };
+  const logout = () => {
+    setToken(null);
+    setUserId(null);
+    setTokenExpiration(null);
+  };
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="auth" element={<AuthPage />} />
-        <Route path="events" element={<EventsPage />} />
-        <Route path="bookings" element={<BookingsPage />} />
-        <Route path="/" element={<Navigate to="/auth" />} />
-      </Routes>
+      <AuthContext.Provider
+        value={{
+          token: token,
+          userId: userId,
+          tokenExpiration: tokenExpiration,
+          login: login,
+          logout: logout,
+        }}
+      >
+        <MainNavigation />
+        <main className={"main__content"}>
+          <Routes>
+            {!token && <Route path="/" element={<Navigate to="/auth" />} />}
+            {!token && <Route path="auth" element={<AuthPage />} />}
+
+            {token && <Route path="/" element={<Navigate to="/events" />} />}
+            {token && <Route path="auth" element={<Navigate to="/events" />} />}
+            {token && <Route path="bookings" element={<BookingsPage />} />}
+
+            <Route path="events" element={<EventsPage />} />
+          </Routes>
+        </main>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./MainNavigation.css";
+import AuthContext from "../../context/auth-context";
+
+const mainNavigation = () => (
+  <AuthContext.Consumer>
+    {(context) => {
+      return (
+        <header className={"main-navigation"}>
+          <div className={"main-navigation__logo"}>
+            <h1>Easy event</h1>
+          </div>
+          <nav className={"main-navigation__item"}>
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to={"/auth"}>Authentication</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to={"/events"}>Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to={"/bookings"}>Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
+);
+
+export default mainNavigation;
